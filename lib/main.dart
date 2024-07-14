cfb602dcc339e2fbb2985780e87c7de5a12417f3
Poomrapee Koonyosying <poompepee@hotmail.com>
2024-07-14 14:43:35 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:login_register_app/screen/home_screen.dart';
+import 'package:login_register_app/screen/login_screen.dart';
+import 'package:login_register_app/screen/register_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/register': (context) => const RegisterScreen(),
+        '/home': (context) => const HomeScreen(),
+      },
+    );
+  }
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,104 @@
+import 'dart:convert';
+
+import 'package:card_swiper/card_swiper.dart';
+import 'package:login_register_app/service/auth_service.dart';
+import 'package:login_register_app/service/page_service.dart';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../config/app.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<dynamic> banners = [];
+  List<dynamic> pages = [];
+  Future<void> fetchBanners() async {
+    try {
+      final response = await http.get(Uri.parse('$API_URL/api/banners'));
+      final banners = jsonDecode(response.body);
+      print(banners);
+      setState(() {
+        this.banners = banners;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> fetchPages() async {
+    try {
+      List<dynamic> pages = await PageService.fetchPage();
+      setState(() {
+        this.pages = pages;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    AuthService.checkLogin().then((loggedIn) {
+      if (!loggedIn) {
+        Navigator.of(context).pushReplacementNamed('/login');
+      }
+    });
+
+    fetchBanners();
+    fetchPages();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: pages.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(pages[index]['title']),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 250,
+            child: Swiper(
+              autoplay: true,
+              itemCount: banners.length,
+              itemBuilder: (context, index) {
+                return Image.network(
+                  '$API_URL/${banners[index]['imageUrl']}',
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
